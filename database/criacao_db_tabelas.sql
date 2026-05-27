04e2e64edcb0e6f3c1fcc7a5d6bb2f1aeb639521
Igor <mmesquita.igor@gmail.com>
2026-05-27 18:12:57 -0300

    Alterações backup dados não rotulados

--- database/criacao_db_tabelas.sql
+++ database/criacao_db_tabelas.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE "CyberbullyingDetection"
 	WITH
 	OWNER = postgres
-	ENCODING = 'UFT8'
+	ENCODING = 'UTF8'
 	LOCALE_PROVIDER = 'icu'
 	ICU_LOCALE = 'en-US'
+	TEMPLATE = template0
 	TABLESPACE = pg_default
 	CONNECTION LIMIT = -1
 	IS_TEMPLATE = False;
+
+CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE dados_rotulados (
     id SERIAL PRIMARY KEY,
@@ -18,7 +21,11 @@ CREATE TABLE dados_rotulados (
 CREATE TABLE dados_nao_rotulados (
 	id SERIAL PRIMARY KEY,
 	tweet TEXT,
-	embedding VECTOR(384)
+	embedding VECTOR(384),
+	rotulo VARCHAR(30),
+	indice_original INT
 );
 
-CREATE INDEX ON dados USING hnsw (embedding vector_cosine_ops);
+CREATE INDEX ON dados_rotulados USING hnsw (embedding vector_cosine_ops);
+
+
